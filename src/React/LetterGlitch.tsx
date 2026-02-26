96b51eb71f452f8b8735be131ccfc0181af8c733
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-02-26 08:59:07 -0300

    fix: :bug: fixing deployment settings

--- src/React/LetterGlitch.tsx
+++ src/React/LetterGlitch.tsx
@@ -104,17 +104,17 @@ const LetterGlitch = ({
 
   const hexToRgb = (hex: string) => {
     const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+    hex = hex.replace(shorthandRegex, (r, g, b) => {
       return r + r + g + g + b + b;
     });
 
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result
       ? {
-          r: parseInt(result[1], 16),
-          g: parseInt(result[2], 16),
-          b: parseInt(result[3], 16),
-        }
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
       : null;
   };
 
